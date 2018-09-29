983cff2211455cce7f90007f238f3b984ca1e688
adamszabolcs <szabolcs.adam91@gmail.com>
2018-09-29 15:06:32 +0200

    started writing tests

--- src/com/codecool/uml/overriding/Webshop.java
+++ src/com/codecool/uml/overriding/Webshop.java
@@ -3,6 +3,7 @@ package com.codecool.uml.overriding;
 public class Webshop {
 
     public static void main(String[] args) {
-	// write your code here
+        Order order = new Order();
+                order.pay();
     }
 }
